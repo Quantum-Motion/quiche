79a958d924b51f0283e1af37b964dbe2db3739bb
Vasco Ferreira <vasco.ferreira@quantummotion.tech>
2026-09-23 15:37:30 +0100

    docs: update sphinx docs config

--- docs/conf.py
+++ docs/conf.py
@@ -28,8 +28,7 @@ copyright = "2026, Quantum Motion Technologies Ltd."  # noqa: A001
 
 with (REPO_ROOT / "pyproject.toml").open("rb") as f:
     release = tomllib.load(f)["project"]["version"]
-
-version = release
+    version = ".".join(release.split(".")[:2])
 
 # -- General configuration ---------------------------------------------------
 
@@ -37,23 +36,24 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.intersphinx",
     "sphinx.ext.napoleon",
-    "sphinx.ext.viewcode",
     "sphinx_copybutton",
     "sphinx_design",
     "myst_parser",
 ]
 
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "README.md"]
+add_module_names = False
+python_use_unqualified_type_names = True
 
 # -- Autodoc -----------------------------------------------------------------
 
 autodoc_default_options = {
     "members": True,
     "member-order": "bysource",
-    "show-inheritance": True,
 }
 autodoc_typehints = "description"
 autodoc_typehints_description_target = "documented_params"
+autodoc_typehints_format = "short"
 
 # -- Napoleon ----------------------------------------------------------------
 
@@ -67,7 +67,7 @@ napoleon_custom_sections = [
 
 # -- MyST ----------------------------------------------------------------------
 
-myst_enable_extensions = ["colon_fence", "deflist", "dollarmath"]
+myst_enable_extensions = ["colon_fence", "deflist", "dollarmath", "alert"]
 myst_heading_anchors = 3
 
 # -- Intersphinx -------------------------------------------------------------
@@ -80,31 +80,11 @@ intersphinx_mapping = {
 
 # -- HTML output -------------------------------------------------------------
 
-html_theme = "furo"
-html_title = f"QUICHE {version}"
+html_theme = "shibuya"
 html_static_path = ["_static"]
 html_theme_options = {
-    "source_repository": "https://github.com/Quantum-Motion/quiche/",
-    "source_branch": "main",
-    "source_directory": "docs/",
-    "footer_icons": [
-        {
-            "name": "GitHub",
-            "url": "https://github.com/Quantum-Motion/quiche",
-            "html": (
-                '<svg stroke="currentColor" fill="currentColor" stroke-width="0" '
-                'viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 '
-                "8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 "
-                "0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1"
-                ".13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 "
-                "2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 "
-                "0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64"
-                "-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 "
-                "1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 "
-                "3.95.28.24.53.71.53 1.43 0 1.03-.01 1.87-.01 2.13 0 .21.15.46.55.38A8"
-                '.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path></svg>'
-            ),
-            "class": "",
-        },
-    ],
+    "nav_socials": [],
+    "github_url": "https://github.com/Quantum-Motion/quiche",
+    "show_ai_links": False,
 }
+html_baseurl = "https://quantum-motion.github.io/quiche/"
